9441c6757482b35fc9d69ab0adeabf783df3ea2e
Stuart <32797526+stunoir@users.noreply.github.com>
2021-12-30 19:28:08 +0000

    add create listing form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Category from './pages/Category'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
           <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
           <Route path='/forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
+          <Route path='/create-listing' element={<CreateListing></CreateListing>}></Route>
         </Routes>
         <NavBar></NavBar>
       </Router>
